62818e9f65da3722452334a04d247bfcb3878416
BuildTools <benjamin.auzanneau.tm@gmail.com>
2017-02-23 19:57:41 +0100

    up

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ let request = require("request"),
   config = require('./pizzaModules/pizzaConfig.js'),
   dataObject = require('./pizzaModules/pizzaDataObject.js');
 
-console.log(dataObject);
+//console.log(dataObject);
+console.log(dataObject.createEndpointObject());
 
 
 
--- pizzaModules/pizzaDataObject.js
+++ pizzaModules/pizzaDataObject.js
@@ -1,8 +1,26 @@
+let config = require('./pizzaConfig.js');
+
+let endpointModel = {
+    l: 0,
+    k: 0,
+    aC: [], // de taille k
+    aL: []  // de taille k
+}
+
 module.exports.dataObject = {
     v: 0,
     e: 0,
     r: 0,
     c: 0,
     x: 0,
-    s: []
+    s: [],
+    aE: [],
+    aRv: [],
+    aRe: [],
+    aRn: []
 };
+
+module.exports.createEndpointObject = function(){
+    return config.utils.clone(endpointModel);
+}
+
