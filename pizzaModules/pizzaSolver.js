f9482be47ce31984f59156d6160590bba5fe311f
Brack0 <denis.souron@laposte.net>
2017-02-23 21:27:44 +0100

    write file example

--- pizzaModules/pizzaSolver.js
+++ pizzaModules/pizzaSolver.js
@@ -40,3 +40,11 @@ cacheResult.forEach(function(cache, index){
 
 console.log(output);
 
+var fs = require('fs');
+fs.writeFile("/tmp/test", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
